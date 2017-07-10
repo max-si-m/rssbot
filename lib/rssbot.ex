da31ce7d5b3bbbb1eb2ac00e261bf0ba854ade4a
Maxim Dzhuliy <mak7.dj@gmail.com>
2017-07-10 22:55:29 +0300

    Initial commit

---
+++ lib/rssbot.ex
@@ -0,0 +1,18 @@
+defmodule Rssbot do
+  @moduledoc """
+  Documentation for Rssbot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rssbot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rssbot_test.exs
@@ -0,0 +1,8 @@
+defmodule RssbotTest do
+  use ExUnit.Case
+  doctest Rssbot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
